8efd76a85c118e64d44e8e06b4c94eb8db75bdea
seungwoo <tmddn645@naver.com>
2021-01-19 13:40:52 +0900

    Update my code

--- text_processing.py
+++ text_processing.py
@@ -33,8 +33,8 @@ def normalize(input_string):
              >>> tp.normalize(input_string2)
              'extra space'
     """
-    normalized_string = None
-    return normalized_string
+    return ' '.join(input_string.lower().split())
+
 
 
 def no_vowels(input_string):
@@ -58,5 +58,5 @@ def no_vowels(input_string):
             >>> tp.normalize(input_string2)
             ''W lv Pythn!'
     """
-    no_vowel_string = None
-    return no_vowel_string
+    table = str.maketrans('aeiou','     ')
+    return input_string.translate(table)
